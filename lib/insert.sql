6c31421f2c1fbf8491cb48e1fa6a7a3dc412e918
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-09-13 00:02:20 +0300

    SQL CRUD AND QUERIES

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",23,"M","white","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy",24,"F","white","sijui",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha",22,"F","brown","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",24,"M","brown","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa",2,"F","white","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch",2,"M","white","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy",23,"F","white","sijui",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,23,"M","white","sijui",1);
